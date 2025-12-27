ab09c9bad68d5807b0574db8b3e95df18b18a9e1
Alexander Piletskiy <a.turnier.9@gmail.com>
2025-12-27 11:44:53 +0200

    feat(App): add base layout structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+// get all css-files (.css)
+import "./assets/styles/index.css";
+
+// get components for App
+import { Header, Footer } from "./models/barrel/forApp.js";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>MAIN</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
