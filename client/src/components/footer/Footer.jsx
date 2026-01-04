83c1abb1daf9f7b497fef979869532308f36dc28
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-04 22:46:30 +0200

    feat(client): add Header and Footer components

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+// base component for <footer> tag
+
+function Footer() {
+  return <footer>FOOTER</footer>;
+}
+
+export default Footer;
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+// base component for <header> tag
+
+function Header() {
+  return <header>HEADER</header>;
+}
+
+export default Header;
