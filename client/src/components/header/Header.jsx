caec888e8c8ecc03869a6369d27c1f5501e339be
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-05 09:47:36 +0200

    feat(client): split Header into logical subcomponents

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,17 @@
 // base component for <header> tag
 
+import Logo from "./logo/Logo.jsx";
+import Menu from "./menu/Menu.jsx";
+import Mini from "./mini/Mini.jsx";
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header>
+      <Logo />
+      <Menu />
+      <Mini />
+    </header>
+  );
 }
 
 export default Header;
