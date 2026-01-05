9b84e4ddb91db0fc4f466d0520ddf4f95df69361
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-05 22:05:24 +0200

    feat(client): add BaseButton component

---
+++ client/src/components/ui/buttons/BaseButton.jsx
@@ -0,0 +1,7 @@
+// Template for different button variants (square, circle etc.)
+
+const BaseButton = function BaseButton({ children }) {
+  return <button>{children}</button>;
+};
+
+export default BaseButton;
