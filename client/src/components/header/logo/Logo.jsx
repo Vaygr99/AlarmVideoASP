f775d602654d77cca526b680a6aa2572c9591596
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-05 23:02:31 +0200

    feat(header): add menu button to logo area

--- client/src/components/header/logo/Logo.jsx
+++ client/src/components/header/logo/Logo.jsx
@@ -1,12 +1,17 @@
 // Displays logo sector inside the header
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import Title from "../../ui/other/title/Title.jsx";
+import SquareIconButton from "../../ui/buttons/squareIconButton/SquareIconButton.jsx";
 
 import styles from "./Logo.module.css";
 
 function Logo() {
   return (
     <div className={styles.container}>
-      <div>MENU</div>
+      <div>
+        <SquareIconButton icon={faBars} className="top-square-button" />
+      </div>
       <Title />
     </div>
   );
