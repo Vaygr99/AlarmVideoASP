be257259b66ead45ac196fcfea4af5aa52c63a8c
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-05 16:32:22 +0200

    feat(header): add logo to Title

--- client/src/components/ui/other/title/Title.jsx
+++ client/src/components/ui/other/title/Title.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 
+import logo from "../../../../assets/images/logo.svg";
+
 function Title() {
   return (
     <Link to="/">
-      <div></div>
+      <div>
+        <img alt={logo} src={logo} />
+      </div>
       <section>
         <p>
           ФОП Пiлецький О.С.
