010f63eb0fe9f1037f29688801e4f3c10cdad6b0
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-06 16:55:19 +0200

    feat(header): add Lang component

---
+++ client/src/components/header/mini/lang/Lang.jsx
@@ -0,0 +1,12 @@
+import LangButton from "../../../ui/buttons/langButton/LangButton.jsx";
+
+function Lang() {
+  return (
+    <div id="mini-lang">
+      <LangButton text="RU" />
+      <LangButton text="UA" />
+    </div>
+  );
+}
+
+export default Lang;
