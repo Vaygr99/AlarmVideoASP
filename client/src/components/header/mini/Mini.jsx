223e337e54683bdff947439f6d913002a53727f2
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-06 16:34:13 +0200

    feat(header): split Mini into logical parts

--- client/src/components/header/mini/Mini.jsx
+++ client/src/components/header/mini/Mini.jsx
@@ -1,5 +1,15 @@
+import Admin from "./admin/Admin.jsx";
+import Phone from "./phone/Phone.jsx";
+import Theme from "./Theme.jsx";
+
 function Mini() {
-  return <div>MINI</div>;
+  return (
+    <div>
+      <Theme />
+      <Phone />
+      <Admin />
+    </div>
+  );
 }
 
 export default Mini;
