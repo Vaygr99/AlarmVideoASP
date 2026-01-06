36890eb17a1555797ee047b7b9cbd5c457e662e5
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-06 16:58:13 +0200

    feat(header): add lang buttons to mini area

--- client/src/components/header/mini/Mini.jsx
+++ client/src/components/header/mini/Mini.jsx
@@ -1,10 +1,12 @@
 import Admin from "./admin/Admin.jsx";
 import Phone from "./phone/Phone.jsx";
 import Theme from "./Theme.jsx";
+import Lang from "./lang/Lang.jsx";
 
 function Mini() {
   return (
     <div>
+      <Lang />
       <Theme />
       <Phone />
       <Admin />
