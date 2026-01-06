44d6f29e0ceed84676bb9e242e17ee0728efd20d
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-06 23:39:02 +0200

    feat(header): add menu area styles

--- client/src/components/header/menu/Menu.jsx
+++ client/src/components/header/menu/Menu.jsx
@@ -1,5 +1,24 @@
+// Includes service button (+ menu) and a link (about us)
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+import IconTextButton from "../../ui/buttons/iconTextButton/IconTextButton.jsx";
+import LinkButton from "../../ui/buttons/linkButton/LinkButton.jsx";
+
+import styles from "./Menu.module.css";
+
 function Menu() {
-  return <div>MENU</div>;
+  return (
+    <div id="menu" className={styles.container}>
+      <div>
+        <IconTextButton
+          icon={faChevronDown}
+          text="Services"
+          className={`services-button`}
+        />
+      </div>
+      <LinkButton to="/about" text="About us" />
+    </div>
+  );
 }
 
 export default Menu;
