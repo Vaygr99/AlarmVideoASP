bf8a4e05b51f84fdbd075b1229918169dea8eba7
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-07 19:38:40 +0200

    feat(header): add services menu to menu area

--- client/src/components/header/menu/Menu.jsx
+++ client/src/components/header/menu/Menu.jsx
@@ -1,22 +1,30 @@
-// Includes service button (+ menu) and a link (about us)
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
 
 import IconTextButton from "../../ui/buttons/iconTextButton/IconTextButton.jsx";
 import LinkButton from "../../ui/buttons/linkButton/LinkButton.jsx";
+import useOutsideClick from "../../ui/menu/useOutsideClick.js";
+import ServiceMenu from "../../ui/menu/serviceMenu/ServiceMenu.jsx";
 
 import styles from "./Menu.module.css";
 
 // Menu area (in top <header>)
 function Menu() {
+  const [open, setOpen] = useState(true);
+  const wrapperRef = useRef(null);
+
+  useOutsideClick([wrapperRef], () => setOpen(false));
+
   return (
     <div id="menu" className={styles.container}>
       {/* Button + services menu */}
-      <div>
+      <div ref={wrapperRef} onClick={() => setOpen((prev) => !prev)}>
         <IconTextButton
           icon={faChevronDown}
           text="Services"
           className={`services-button`}
         />
+        {open && <ServiceMenu />}
       </div>
       <LinkButton to="/about" text="About us" />
     </div>
