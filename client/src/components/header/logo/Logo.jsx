3fd25cf53fed40362e0b17541e25d6286fa7dfe6
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-07 17:21:30 +0200

    feat(header): add menu to logo area

--- client/src/components/header/logo/Logo.jsx
+++ client/src/components/header/logo/Logo.jsx
@@ -1,18 +1,23 @@
 // Displays logo sector inside the header
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 import Title from "../../ui/other/title/Title.jsx";
 import SquareIconButton from "../../ui/buttons/squareIconButton/SquareIconButton.jsx";
+import LogoMenu from "../../ui/menu/logoMenu/LogoMenu.jsx";
 
 import styles from "./Logo.module.css";
 
 // Logo area (in top <header>)
 function Logo() {
+  const [open, setOpen] = useState(true);
+
   return (
     <div id="logo" className={styles.container}>
       {/* button + menu (services and about us) */}
       <div>
         <SquareIconButton icon={faBars} className="top-square-button" />
+        {open && <LogoMenu />}
       </div>
       <Title />
     </div>
