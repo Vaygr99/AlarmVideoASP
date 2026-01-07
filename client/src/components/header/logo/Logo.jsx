78dfd52f4168d9333d367015753671dd20a91805
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-07 19:07:34 +0200

    feat(header): use menu display in logo area

--- client/src/components/header/logo/Logo.jsx
+++ client/src/components/header/logo/Logo.jsx
@@ -1,21 +1,25 @@
 // Displays logo sector inside the header
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Title from "../../ui/other/title/Title.jsx";
 import SquareIconButton from "../../ui/buttons/squareIconButton/SquareIconButton.jsx";
 import LogoMenu from "../../ui/menu/logoMenu/LogoMenu.jsx";
+import useOutsideClick from "../../ui/menu/useOutsideClick.js";
 
 import styles from "./Logo.module.css";
 
 // Logo area (in top <header>)
 function Logo() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const wrapperRef = useRef(null);
+
+  useOutsideClick([wrapperRef], () => setOpen(false));
 
   return (
     <div id="logo" className={styles.container}>
       {/* button + menu (services and about us) */}
-      <div>
+      <div ref={wrapperRef} onClick={() => setOpen((prev) => !prev)}>
         <SquareIconButton icon={faBars} className="top-square-button" />
         {open && <LogoMenu />}
       </div>
