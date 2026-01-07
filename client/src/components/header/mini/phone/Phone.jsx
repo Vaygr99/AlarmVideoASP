12944d534ec7ec9836e9a2c641a8662c86f133fc
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-07 23:20:06 +0200

    feat(header): use phones menu display in mini area

--- client/src/components/header/mini/phone/Phone.jsx
+++ client/src/components/header/mini/phone/Phone.jsx
@@ -1,19 +1,27 @@
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import SquareIconButton from "../../../ui/buttons/squareIconButton/SquareIconButton.jsx";
 import PhonesMenu from "../../../ui/menu/phonesMenu/PhonesMenu.jsx";
+import useOutsideClick from "../../../ui/menu/useOutsideClick.js";
 
 import styles from "./Phone.module.css";
 
 // Phone container from mini area (button + menu)
 function Phone() {
   const [open, setOpen] = useState(true);
+  const wrapperRef = useRef(null);
+
+  useOutsideClick([wrapperRef], () => setOpen(false));
 
   return (
     <>
       {/* Button + phones menu */}
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        ref={wrapperRef}
+        onClick={() => setOpen((prev) => !prev)}
+      >
         <SquareIconButton icon={faPhone} className="top-square-button" />
         {open && <PhonesMenu />}
       </div>
