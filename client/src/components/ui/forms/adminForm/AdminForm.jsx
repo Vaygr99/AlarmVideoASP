c9522778d762b9a8ec8cf7281be88acf12a1a27f
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 17:31:36 +0200

    feat(header): add admin form styles

--- client/src/components/ui/forms/adminForm/AdminForm.jsx
+++ client/src/components/ui/forms/adminForm/AdminForm.jsx
@@ -1,10 +1,12 @@
 import PasswordInput from "./passwordInput/PasswordInput.jsx";
 import Submit from "./submit/Submit.jsx";
 
+import styles from "./AdminForm.module.css";
+
 // Admin form from mini area (pussword input and submit/cancel buttons)
 function AdminForm({ ...props }) {
   return (
-    <form>
+    <form className={styles.container}>
       <h2>Логин</h2>
       <PasswordInput />
       <Submit {...props} />
