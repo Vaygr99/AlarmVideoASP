b80490cb3bb39f3dbe822e70dfcf5ffd1ee69aac
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 18:50:25 +0200

    feat(header): use admin form display in mini area

--- client/src/components/header/mini/admin/Admin.jsx
+++ client/src/components/header/mini/admin/Admin.jsx
@@ -1,19 +1,23 @@
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import SquareIconButton from "../../../ui/buttons/squareIconButton/SquareIconButton.jsx";
 import AdminForm from "../../../ui/forms/adminForm/AdminForm.jsx";
+import useOutsideClick from "../../../ui/menu/useOutsideClick.js";
 
 // Admin container from mini area (button + form)
 function Admin() {
   const [open, setOpen] = useState(true);
+  const wrapperRef = useRef(null);
 
+  useOutsideClick([wrapperRef], () => setOpen(false));
   return (
     <>
       {/* button + authorization form */}
-      <div id="mini-admin">
+      <div id="mini-admin" ref={wrapperRef}>
         <SquareIconButton
           className="top-square-button"
+          onClick={() => setOpen((prev) => !prev)}
           icon={faUserAstronaut}
         />
         {open && <AdminForm onClick={() => setOpen(false)} />}
