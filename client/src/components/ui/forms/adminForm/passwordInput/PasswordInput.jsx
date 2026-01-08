826cdbccf5f0a89fc52bf180aa32c5e18e8fde54
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 17:46:22 +0200

    feat(header): add password input styles

--- client/src/components/ui/forms/adminForm/passwordInput/PasswordInput.jsx
+++ client/src/components/ui/forms/adminForm/passwordInput/PasswordInput.jsx
@@ -7,10 +7,12 @@ import {
 import BaseInput from "../../../inputs/BaseInput.jsx";
 import SquareIconButton from "../../../buttons/squareIconButton/SquareIconButton.jsx";
 
+import styles from "./PasswordInput.module.css";
+
 // Container for entering admin password for admin form in mini area
 function PasswordInput() {
   return (
-    <div>
+    <div className={styles.container}>
       <span>10</span>
       <BaseInput placeholder="пароль" />
       <SquareIconButton icon={faEye} />
