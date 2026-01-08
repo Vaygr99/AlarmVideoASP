0698d4afd4735927bbe348b3fc214f83565c8efb
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 17:52:12 +0200

    feat(header): add submit styles in admin form

--- client/src/components/ui/forms/adminForm/submit/Submit.jsx
+++ client/src/components/ui/forms/adminForm/submit/Submit.jsx
@@ -2,10 +2,12 @@ import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import SquareIconButton from "../../../buttons/squareIconButton/SquareIconButton.jsx";
 
+import styles from "./Submit.module.css";
+
 // Container for confirm/cancel buttons of admin form in mini area
 function Submit({ ...props }) {
   return (
-    <div>
+    <div className={styles.container}>
       <SquareIconButton {...props} icon={faXmark} />
       <SquareIconButton icon={faCheck} buttonType="submit" />
     </div>
