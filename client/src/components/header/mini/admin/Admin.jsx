868fdc1e81e97f04178a600a12624a7db005d1a1
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 17:23:05 +0200

    feat(header): add admin form to mini area

--- client/src/components/header/mini/admin/Admin.jsx
+++ client/src/components/header/mini/admin/Admin.jsx
@@ -1,9 +1,13 @@
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 import SquareIconButton from "../../../ui/buttons/squareIconButton/SquareIconButton.jsx";
+import AdminForm from "../../../ui/forms/adminForm/AdminForm.jsx";
 
 // Admin container from mini area (button + form)
 function Admin() {
+  const [open, setOpen] = useState(true);
+
   return (
     <>
       {/* button + authorization form */}
@@ -12,6 +16,7 @@ function Admin() {
           className="top-square-button"
           icon={faUserAstronaut}
         />
+        {open && <AdminForm onClick={() => setOpen(false)} />}
       </div>
     </>
   );
