a5fc0c8dc41d297c4777244278746df1efdc0431
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-08 20:41:04 +0200

    feat(router): add pages navigation structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // all css-files in project (.css)
 import "./assets/styles/index.css";
 // Components list for App
-import { Header, Footer } from "./models/unifications/forApp.js";
+import {
+  Header,
+  Footer,
+  Burglar,
+  Mount,
+  Home,
+  Video,
+  About,
+  Fire,
+  Admin,
+  Offer,
+  Info,
+  Customers,
+  Edit,
+} from "./models/unifications/forApp.js";
 
 // Base client component
 function App() {
@@ -11,7 +25,24 @@ function App() {
     <div>
       <BrowserRouter>
         <Header />
-        <main id="main">MAIN</main>
+        <main id="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="burglar" element={<Burglar />} />
+            <Route path="video" element={<Video />} />
+            <Route path="mount" element={<Mount />} />
+            <Route path="fire" element={<Fire />} />
+            <Route path="about" element={<About />} />
+            {/* admin pages */}
+            <Route path="admin" element={<Admin />}>
+              <Route path="info" element={<Info />} />
+              <Route index element={<Navigate to="info" replace />} />
+              <Route path="customers" element={<Customers />} />
+              <Route path="edit" element={<Edit />} />
+              <Route path="offer" element={<Offer />} />
+            </Route>
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </div>
--- client/src/models/unifications/forApp.js
+++ client/src/models/unifications/forApp.js
@@ -1,2 +1,16 @@
 export { default as Header } from "../../components/header/Header.jsx";
 export { default as Footer } from "../../components/footer/Footer.jsx";
+
+// export client pages
+export { default as Home } from "../../components/pages/home/Home.jsx";
+export { default as Burglar } from "../../components/pages/burglar/Burglar.jsx";
+export { default as Fire } from "../../components/pages/fire/Fire.jsx";
+export { default as Video } from "../../components/pages/video/Video.jsx";
+export { default as Mount } from "../../components/pages/mount/Mount.jsx";
+export { default as About } from "../../components/pages/about/About.jsx";
+// export admin pages
+export { default as Admin } from "../../components/pages/admin/Admin.jsx";
+export { default as Info } from "../../components/pages/admin/info/Info.jsx";
+export { default as Customers } from "../../components/pages/admin/customers/Customers.jsx";
+export { default as Edit } from "../../components/pages/admin/edit/Edit.jsx";
+export { default as Offer } from "../../components/pages/admin/offer/Offer.jsx";
