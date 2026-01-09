668216b21a1e3f7f8cac71f9f8bbc885e16bc950
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-09 13:04:04 +0200

    feat(server): get data from db for edit section

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const PORT = 5000;
+
+const PORT = 4000;
+
+const { dbConnection } = require("./db.js");
 const app = express();
+
+// Get data from clients and devices for edit section in admin area
+app.get("/edit-data", async (req, res) => {
+  try {
+    const db = await dbConnection();
+    const clients = await db.collection("clients").find().toArray();
+    const devices = await db.collection("devices").find().toArray();
+    res.status(200).json({ clients, devices });
+  } catch (error) {
+    console.error("Error getting data for edit section:", error.message);
+    res
+      .status(500)
+      .json({ error: "Server error while download data for edit section" });
+  }
+});
 
 app.listen(PORT, () => {
   console.dir(`Server is running on port ${PORT}`);
