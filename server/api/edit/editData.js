03097f489ab8061c858176c5a37d04efc7e5c2cf
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-09 22:17:33 +0200

    feat(server): add PUT endpoint for edit section

--- server/api/edit/editData.js
+++ server/api/edit/editData.js
@@ -4,6 +4,8 @@ function editData(app, db) {
   // app - express object
   // db - mongodb object
 
+  const { ObjectId } = require("mongodb");
+
   // Get data from clients and devices collections
   app.get("/edit-data", async (req, res) => {
     try {
@@ -33,6 +35,35 @@ function editData(app, db) {
     } catch (error) {
       console.error("Error adding client:", error.message);
       res.status(500).json({ error: "Server error while adding client" });
+    }
+  });
+
+  // Update client
+  app.put("/edit-data/clients/update/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updateData = req.body;
+      // If data provided for update
+      if (!updateData || Object.keys(updateData).length === 0) {
+        return res.status(400).json({ error: "No data provided for update" });
+      }
+      // If id is valid
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Invalid client ID" });
+      }
+      // update selected client
+      const updated = await db
+        .collection("clients")
+        .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+      // if no client was found
+      if (updated.matchedCount === 0) {
+        return res.status(404).json({ error: "Client not found" });
+      }
+
+      res.status(200).json({ message: "Client updated successfully" });
+    } catch (err) {
+      console.error("Error updating client:", err.message);
+      res.status(500).json({ error: "Server error" });
     }
   });
 }
