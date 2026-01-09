d83c9d323ab624c33e3e7d4a487c9544ccd50743
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-09 23:13:57 +0200

    feat(server): add DELETE endpoint (client) for edit section

--- server/api/edit/editData.js
+++ server/api/edit/editData.js
@@ -6,12 +6,13 @@ function editData(app, db) {
 
   const { ObjectId } = require("mongodb");
 
-  // Get data from clients and devices collections
+  // Get data from clients, devices and offers collections
   app.get("/edit-data", async (req, res) => {
     try {
       const clients = await db.collection("clients").find().toArray();
       const devices = await db.collection("devices").find().toArray();
-      res.status(200).json({ clients, devices });
+      const offers = await db.collection("offers").find().toArray();
+      res.status(200).json({ clients, devices, offers });
     } catch (error) {
       console.error("Error getting data for edit section:", error.message);
       res
@@ -24,8 +25,7 @@ function editData(app, db) {
   app.post("/edit-data/clients/new", async (req, res) => {
     try {
       const { name, phone, info } = req.body;
-
-      // If data is incorrect
+      // Verify data
       if (!verifyClients(req.body)) {
         return res.status(400).json({ error: "Incorrect client data" });
       }
@@ -43,19 +43,19 @@ function editData(app, db) {
     try {
       const { id } = req.params;
       const updateData = req.body;
-      // If data provided for update
+      // Check for data availability
       if (!updateData || Object.keys(updateData).length === 0) {
         return res.status(400).json({ error: "No data provided for update" });
       }
-      // If id is valid
+      // Validate id
       if (!ObjectId.isValid(id)) {
         return res.status(400).json({ error: "Invalid client ID" });
       }
-      // update selected client
+      // Update selected client
       const updated = await db
         .collection("clients")
         .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
-      // if no client was found
+      // Check for client availability
       if (updated.matchedCount === 0) {
         return res.status(404).json({ error: "Client not found" });
       }
@@ -63,6 +63,30 @@ function editData(app, db) {
       res.status(200).json({ message: "Client updated successfully" });
     } catch (err) {
       console.error("Error updating client:", err.message);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+
+  // delete client
+  app.delete("/edit-data/clients/delete/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      // Validate id
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Invalid client ID" });
+      }
+      // Delete client
+      const result = await db
+        .collection("clients")
+        .deleteOne({ _id: new ObjectId(id) });
+      if (result.deletedCount === 0) {
+        return res.status(404).json({ error: "Client not found" });
+      }
+      // Delete related offers
+      await db.collection("offers").deleteMany({ clientId: new ObjectId(id) });
+      res.status(200).json({ message: "Client deleted successfully" });
+    } catch (err) {
+      console.error("Error deleting client:", err.message);
       res.status(500).json({ error: "Server error" });
     }
   });
