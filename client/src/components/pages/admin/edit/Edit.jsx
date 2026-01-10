9992067f107468b2c4c85ca2ad390150987486ae
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-10 11:37:04 +0200

    feat(admin): compose base layout for edit page

--- client/src/components/pages/admin/edit/Edit.jsx
+++ client/src/components/pages/admin/edit/Edit.jsx
@@ -1,5 +1,22 @@
+// Page for working with clients and devices
 function Edit() {
-  return <div>Edit</div>;
+  return (
+    <div id="edit">
+      {/* Clients area */}
+      <div>
+        <div>
+          <h2>Клiєнти</h2>
+        </div>
+      </div>
+
+      {/* Devices area */}
+      <div>
+        <div>
+          <h2>Обладнання</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Edit;
