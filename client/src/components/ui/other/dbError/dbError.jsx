4bd1811b8b5dc3764578999fde3003ba60858654
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-12 20:31:14 +0200

    feat(admin): add db operations error section

---
+++ client/src/components/ui/other/dbError/dbError.jsx
@@ -0,0 +1,18 @@
+import styles from "./dbError.module.css";
+
+// set message, when db operations error
+function dbError({ message }) {
+  return (
+    <div id="loading-error" className={styles.container}>
+      <p>{message}</p>
+      <div>
+        <div className={styles.errorCirle}></div>
+        <div className={styles.errorLeftEye}></div>
+        <div className={styles.errorRightEye}></div>
+        <div className={styles.errorMouth}></div>
+      </div>
+    </div>
+  );
+}
+
+export default dbError;
--- client/src/models/dbOperations/getDbData.js
+++ client/src/models/dbOperations/getDbData.js
@@ -1,13 +1,12 @@
 // Get collections (clients, devices, offers) for edit page
-async function getDataForEditPage(setData, setLoading) {
+async function getDataForEditPage(setData, setLoading, setDbError) {
   try {
     const result = await fetch("http://localhost:4000/edit-data");
     if (!result.ok) throw new Error("Error");
     const data = await result.json();
     setData(data);
   } catch (err) {
-    console.log(err.message);
-    //setLoadingError(err.message);
+    setDbError(err.message);
   } finally {
     // remove loading animation after 1 second
     setTimeout(() => setLoading(false), 1000);
