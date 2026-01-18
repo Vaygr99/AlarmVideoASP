299dfd2f303c69b931879c2da1af6188141ee790
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-18 19:59:03 +0200

    feat(admin): add content for model row in device section

--- client/src/components/pages/admin/edit/devices/modelsList/Model.jsx
+++ client/src/components/pages/admin/edit/devices/modelsList/Model.jsx
@@ -1,8 +1,20 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
+import SquareIconButton from "../../../../../ui/buttons/squareIconButton/SquareIconButton.jsx";
+import UncontrolledIconInput from "../../../../../ui/inputs/iconInput/UncontrolledIconInput.jsx";
+
 function Model() {
-  return <div>Model</div>;
+  return (
+    <div>
+      <SquareIconButton icon={faPlus} className="top-square-button" />
+      <UncontrolledIconInput
+        //ref={modelRef}
+        placeholder="Модель"
+        icon={faTrashCan}
+      />
+      <SquareIconButton icon={faMinus} className="top-square-button" />
+    </div>
+  );
 }
 
 export default Model;
