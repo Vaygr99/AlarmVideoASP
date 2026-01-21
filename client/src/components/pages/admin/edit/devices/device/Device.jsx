ae52fb73632700bf58520f33644257db1a25924f
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-21 20:02:38 +0200

    feat(admin): add function to update found devices (client side)

--- client/src/components/pages/admin/edit/devices/device/Device.jsx
+++ client/src/components/pages/admin/edit/devices/device/Device.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect, useRef } from "react";
 
 import UncontrolledIconInput from "../../../../../ui/inputs/iconInput/UncontrolledIconInput.jsx";
 import ModelsList from "../modelsList/ModelsList.jsx";
+import TextButton from "../../../../../ui/buttons/textButton/TextButton.jsx";
+
+import { updateDevice } from "../../../../../../models/dbOperations/putDbData.js";
 
 function Device({
   data,
@@ -13,6 +16,9 @@ function Device({
 }) {
   const nameRef = useRef(null);
   const [models, setModels] = useState(modelsArray);
+
+  // Update device error
+  const [updateDeviceError, setUpdateDeviceError] = useState(false);
 
   useEffect(() => {
     nameRef.current.value = device.name;
@@ -34,6 +40,26 @@ function Device({
 
       {/* Models list */}
       <ModelsList models={models} setModels={setModels} />
+
+      <div>
+        {/* Update device button */}
+        <TextButton
+          text="Обновить"
+          onClick={() =>
+            updateDevice(
+              {
+                _id: device._id,
+                name: nameRef.current.value,
+                models,
+              },
+              data,
+              setData,
+              setLoading,
+              setUpdateDeviceError
+            )
+          }
+        />
+      </div>
     </div>
   );
 }
--- client/src/models/dbOperations/putDbData.js
+++ client/src/models/dbOperations/putDbData.js
@@ -6,9 +6,6 @@ async function updateClient(
   setLoading,
   setUpdateClientError
 ) {
-  // name - update client name (object, not .current.value)
-  // phone - update client phone
-  // info - update client info (object, not .current.value)
   try {
     setLoading(true);
 
@@ -58,4 +55,60 @@ async function updateClient(
   }
 }
 
-export { updateClient };
+// --------------------------------------------
+
+// Update client on edit page
+async function updateDevice(
+  device = {},
+  data,
+  setData,
+  setLoading,
+  setUpdateDeviceError
+) {
+  try {
+    setLoading(true);
+
+    // update device
+    const result = await fetch(
+      `http://localhost:4000/edit-data/devices/update/${device._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: device.name,
+          models: device.models,
+        }),
+      }
+    );
+
+    // get messages from server
+    const answer = await result.json();
+
+    if (!result.ok) throw new Error(answer.error || "Update device Error");
+
+    // update device of root object
+    setData((prev) => ({
+      ...prev,
+      devices: prev.devices.map((elem) => {
+        return device._id === elem._id
+          ? {
+              _id: device._id,
+              models: device.models,
+            }
+          : elem;
+      }),
+    }));
+    // no error message
+    setUpdateDeviceError(false);
+  } catch (err) {
+    setUpdateDeviceError(true);
+    console.log(err.message);
+  } finally {
+    // remove loading animation
+    setLoading(false);
+  }
+}
+
+export { updateClient, updateDevice };
