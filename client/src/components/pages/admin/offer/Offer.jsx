eb9e02e3e896b97cd171fae0574b6a624a2fcd87
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-22 20:05:50 +0200

    feat(client): get all info from server to offer page

--- client/src/components/pages/admin/offer/Offer.jsx
+++ client/src/components/pages/admin/offer/Offer.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+
+import { getDataForPages } from "../../../../models/dbOperations/getDbData.js";
+
+// Page for creating offers
 function Offer() {
+  // data from server
+  const [data, setData] = useState({});
+  // data loading status
+  const [loading, setLoading] = useState(true);
+  // db operations error message
+  const [dbError, setDbError] = useState("");
+
+  useEffect(() => {
+    getDataForPages(setData, setLoading, setDbError);
+  }, []);
+
+  // if error, when loading data from server
+  if (dbError) {
+    console.error(`Loading error: ${dbError}`);
+    return (
+      <div id="offer">
+        <DbError message="Ошибка загрузки данных..." />
+      </div>
+    );
+  }
+
   return <div>Offer</div>;
 }
 export default Offer;
