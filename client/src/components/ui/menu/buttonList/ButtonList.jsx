6b17efe08a0a542897efb5cf231f15658a3c3c1e
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-23 13:09:42 +0200

    feat(client): add styles for button in ButtonList

--- client/src/components/ui/menu/buttonList/ButtonList.jsx
+++ client/src/components/ui/menu/buttonList/ButtonList.jsx
@@ -1,11 +1,13 @@
 import IconTextButton from "../../buttons/iconTextButton/IconTextButton.jsx";
 import List from "./List.jsx";
 
+import styles from "./ButtonList.module.css";
+
 // Button (with icon and text) for opening list items
 function ButtonList({ text, icon, list }) {
   return (
-    <div>
-      <IconTextButton text={text} icon={icon} />
+    <div className={styles.container}>
+      <IconTextButton className={styles.button} text={text} icon={icon} />
       <List list={list} />
     </div>
   );
