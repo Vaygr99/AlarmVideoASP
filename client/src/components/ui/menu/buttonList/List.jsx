c3279dafe7768cb684e3ba38c2872cbee2725389
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-23 15:37:09 +0200

    feat(client): add styles for list in ButtonList

--- client/src/components/ui/menu/buttonList/List.jsx
+++ client/src/components/ui/menu/buttonList/List.jsx
@@ -1,7 +1,9 @@
+import styles from "./List.module.css";
+
 // List to show different info (not in header area) 
 function List({ list }) {
   return (
-    <nav>
+    <nav className={styles.container}>
       {list.map((elem) => (
         <button><span>{elem}</span></button>
       ))}
