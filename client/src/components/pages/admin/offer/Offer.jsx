bf5bfb1aef974a2e3a9d12516a36c66f66156103
Alexander Piletskiy <a.turnier.9@gmail.com>
2026-01-24 12:31:59 +0200

    feat(client): update title section and add offer object
    In title section added executor input, some style performanse. Also added object to save all offer data.

--- client/src/components/pages/admin/offer/Offer.jsx
+++ client/src/components/pages/admin/offer/Offer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { faChevronDown, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
 
 import DbError from "../../../ui/other/dbError/dbError.jsx";
 import ButtonList from "../../../ui/menu/buttonList/ButtonList.jsx";
@@ -17,8 +18,27 @@ function Offer() {
   const [loading, setLoading] = useState(true);
   // db operations error message
   const [dbError, setDbError] = useState("");
+  // client id, checked in title section
+  const [checkedClientId, setCheckedClientId] = useState("");
+  // new offer
+  const [newOffer, setNewOffer] = useState({
+    title: "",
+    address: "",
+    executor: "",
+    clientId: checkedClientId,
+    rows: [
+      { model: "", price: "", quantity: "", unit: "", name: "", id: uuidv4() },
+    ],
+    road: "",
+    mount: "",
+    project: "",
+    adjustments: [{ name: "", text: "", id: uuidv4() }],
+  });
 
-  let nameRef = useRef("");
+  // references
+  const executorRef = useRef(null);
+  const addressRef = useRef(null);
+  const titleRef = useRef(null);
 
   useEffect(() => {
     getDataForPages(setData, setLoading, setDbError);
@@ -36,8 +56,10 @@ function Offer() {
 
   return (
     <div id="offer" className={styles.container}>
+      <h2>Комерцiйна пропозицiя</h2>
       <div className={styles.title}>
         <div>
+          {/* Client */}
           <span>Заказчик:</span>
           <ButtonList
             text="Заказчик"
@@ -45,21 +67,35 @@ function Offer() {
             list={data?.clients.map((elem) => elem.name)}
           />
         </div>
+        {/* Client's phone */}
         <div>
           <span>Телефон:</span>
           <span>{}</span>
         </div>
+        {/* date of offer creation */}
         <div>
           <span>Дата:</span>
           <span>{new Date().toLocaleDateString()}</span>
         </div>
+        {/* address of object */}
         <div>
           <span>Адрес:</span>
           <div>
             <UncontrolledIconInput
               placeholder="Назва об'єкту"
               icon={faTrashCan}
-              inputRef={nameRef}
+              inputRef={addressRef}
+            />
+          </div>
+        </div>
+        {/* executor of work */}
+        <div>
+          <span>Исполнитель:</span>
+          <div>
+            <UncontrolledIconInput
+              placeholder="Исполнитель"
+              icon={faTrashCan}
+              inputRef={executorRef}
             />
           </div>
         </div>
